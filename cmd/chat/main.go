1a6ff15f7292cb238b17650151034f9d12559624
berteek <merzinalbert@gmail.com>
2023-10-01 20:04:41 +0400

    feat(main): add comments

--- cmd/chat/main.go
+++ cmd/chat/main.go
@@ -6,7 +6,11 @@ import (
 )
 
 func main() {
+    // create logger
     logger := logging.NewZap()
+    // flush logger buffers
     defer logger.Flush()
+
+    // run server with dependecies
     server.Run(logger)
 }
